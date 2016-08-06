6d35447896c0a9ce63c0401827573a61e7152079
Robert Deans <robertdeans@Roberts-MacBook-Pro.local>
2016-08-06 14:47:28 -0400

    added favs

---
+++ Printing Playground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+var favoriteWord = "zenith"
+var favoriteColor = "violet"
+var favoriteMusician = "Prince"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
