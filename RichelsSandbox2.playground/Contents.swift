78b571461e7c11be0c17e116f596d77895fc8391
Richel Cuyler <richelcuyler@icloud.com>
2016-08-07 01:14:04 -0400

    Printing Variables

---
+++ RichelsSandbox2.playground/Contents.swift
@@ -0,0 +1,16 @@
+var favoriteWord = "antidisestablishmentarianism"
+var favoriteColor = "Periwinkle"
+var favoriteMusician = "Prince"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var colorDescription = "\(favoriteColor) is the perfect blend of blue, white, gray, and purple."
+print(colorDescription)
