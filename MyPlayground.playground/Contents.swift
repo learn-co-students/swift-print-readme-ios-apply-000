a0cd2f309a959b100a52f5e3233c9b60b7ec32db
Henry Ly <henryly@gmail.com>
2016-08-09 10:25:18 -0700

    Added variables and print statements
    The quick brown fox jumps over the lazy dog.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+var favoriteWord = "flatiron"
+var favoriteColor = "orange"
+var favoriteMusician = "kanye"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var animalOneColor = "brown"
+var animalOne = "fox"
+var action = "jumps"
+var animalTwo = "dog"
+
+var firstPart = "The quick \(animalOneColor) \(animalOne) \(action)"
+var secondPart = "over the lazy \(animalTwo)"
+
+print("\(firstPart) \(secondPart).")
