263b9e231ce06c59152c9326898eefa1557610b2
Christopher <christopher@ChristophersPro.westell.com>
2016-08-18 08:03:14 -0400

    Finished lesson

---
+++ Print Playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+var favoriteWord = "crespicular"
+var favoriteColor = "green"
+var favoriteMusician = "The Beatles"
+
+print (favoriteWord)
+print (favoriteColor)
+print (favoriteMusician)
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var skySentence = "The sky is \(color)."
+print(skySentence)
