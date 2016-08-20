db78913caa8693f3c2fd97bbe560a3528dc30bb3
Meghan McCandless <meghanmccandless@gmail.com>
2016-08-20 16:30:35 -0400

    print playground

---
+++ Learn.Co. Print.playground/Contents.swift
@@ -0,0 +1,19 @@
+var favoriteWord = "ephemeral"
+var favoriteColor = "navy"
+var favoriteMusician = "Balmorhea"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print("the sky is \(color).")
+
+print("Hi Mom! /my favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var skySentence = "The sky is \(color)."
+print(skySentence)
