b880faa0ab2cb9908c324877a72ab52e44d91757
Arun Mavumkal <arunmavumkal@Aruns-MBP.fios-router.home>
2016-09-05 00:58:34 -0400

    Complete

---
+++ favoritePlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let favoriteWord = "awesome"
+let favoriteColor = "blue"
+let favoriteMusician = "Beach Boys"
+
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("String literal")
+
+let color = "blue"
+
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+
+let skySentence = "The sky is \(color)."
+
+print (skySentence)
