237d8391ab41b91d24dc748b0acc592b4aec6995
annalu <xl2474@columbia.edu>
2016-09-06 19:11:26 -0400

    Complete problems

---
+++ print.playground/Contents.swift
@@ -0,0 +1,15 @@
+var favoriteWord = "happy"
+var favoriteColor = "blue"
+favoriteColor = "yellow"
+var favoriteMusician = "Luhan"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I love my mom!")
+
+print("My mom's favorite color is \(favoriteColor)!")
+
+var colorSentence = "May mom and I have same favorite color, which is \(favoriteColor)!"
+print(colorSentence)
