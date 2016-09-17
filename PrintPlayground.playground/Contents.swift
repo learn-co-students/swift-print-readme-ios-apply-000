e428a8a8dc4f1c25c59e66c1466abb988dcbc448
Behdad Analui <behdad@Behdads-iMac.local>
2016-09-17 12:58:20 -0700

    Removed README and added Print playground

---
+++ PrintPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var favoriteWord = "Hacienda"
+var favoriteColor = "Red"
+var favoriteMusician = "Freddie Mercury"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("My favorite color is \(favoriteColor)")
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+let USPresident = "Obama"
+let hisWife = "Michelle"
+let MyName = "Slim Shady"
+
+print("Hi My name is \(MyName), Hi My name is \(MyName) and \(USPresident) married \(hisWife) in 1990")
+
+var MyFavoritePersonSentence = "My favorite person is \(USPresident)"
+
+print(MyFavoritePersonSentence)
+
+        
