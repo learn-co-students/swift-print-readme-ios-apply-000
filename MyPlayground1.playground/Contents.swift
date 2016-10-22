a194043c09443321b808b30a730653602fbb05dc
Gvayn <mikhailpan@Mikhails-Air.home>
2016-10-22 18:24:27 -0400

    Complete problems

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,17 @@
+var favoriteWord = "Earth"
+var favoriteColour = "Blue"
+var favoriteMusician = "XX"
+
+print (favoriteWord)
+print (favoriteColour)
+print (favoriteMusician)
+
+print ("I'm printing a string in swift")
+print ("Hi mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print ("The sky is \(color).")
+print ("Hi Mom! My favorite word is \(favoriteWord), My favorite color is \(favoriteColour), and my favorite musician is \(favoriteMusician).")
+
+var skySentence = "The sky is \(color)."
+print (skySentence)
