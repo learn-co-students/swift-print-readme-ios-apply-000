f1c6b2f906c62bc2ff445c25aec49ba1e3fee361
ejbronze <edsmacbook@gmail.com>
2016-10-23 20:41:13 -0400

    Done2

--- Print.playground/Contents.swift
+++ Print.playground/Contents.swift
@@ -1,2 +1,12 @@
-let str = "Hello, World"
-str = "Good Bye, World"
+var favoriteWord = "mercurial"
+var favoriteColor = "blue"
+var favoriteMusician = "Nirvana"
+
+print (favoriteWord)
+print (favoriteColor)
+print(favoriteMusician)
+
+let color = "blue"
+print("The sky is \(color).")
+
+print ("Hey Mom, did you know that my favorite color is \(favoriteColor).")
