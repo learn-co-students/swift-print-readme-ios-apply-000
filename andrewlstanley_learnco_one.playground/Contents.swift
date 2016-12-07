5318a64ccc3396d0e42beba5a2a17f3578aa1ff2
AndrewLStanley <andrewlstanley@MU9CD17M-2.local>
2016-12-07 17:54:49 -0600

    These are a few of my favorite things🎼

---
+++ andrewlstanley_learnco_one.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//favorite word
+var favoriteWord = "blitz"
+
+//favorite color
+var favoriteColor = "blue"
+
+//favorite musician
+var favoriteBand = "Bastille"
+
+
+print(favoriteWord)
+print(favoriteBand)
+print(favoriteColor)
+
+print("I'm printing a string in Swift!")
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteBand).")
+
+
+var currentWeather = "cold"
+var weatherSentence = "It is really \(currentWeather) outside right now!"
+
+print(weatherSentence)
+
+var favoriteAnimal = "dog"
+var animalSentence = ("My favorite animal is a \(favoriteAnimal)")
+
+print(animalSentence)
