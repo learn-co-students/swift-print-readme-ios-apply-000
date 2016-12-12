058bd6e23c473ce2cfa090e332c90334f8f64601
Bakhti <bakhti@Bakhtis-MacBook-Air.local>
2016-12-11 21:46:38 -0500

    PrintingValues

---
+++ myprint.playground/Contents.swift
@@ -0,0 +1,19 @@
+var favoriteMusician = "UmaTurman" // Use your own artist
+var favoriteColor = "White" // Use your own color
+var favoriteWord = "Pronto" // Use your special word
+
+print(favoriteMusician)
+print(favoriteColor)
+print(favoriteWord)
+
+print("I'm printing a string in Swift!")
+
+print ("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print ("the sky is \(color)")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var saidRelated = "he just said \(favoriteWord)."
+print (saidRelated)
