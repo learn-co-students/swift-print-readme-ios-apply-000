346a05705a2c38642e7e1174149b27dd3291753b
sejdemi <sejan.miah@gmail.com>
2016-12-13 14:39:17 -0500

    String Interpolation

---
+++ Printing.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var color = "cerulean"
+var artist = "Beyonce"
+var word = "reify"
+
+print(color)
+print(artist)
+print(word)
+
+print("I'm printing a string in Swift!")
+
+print("My favorite word is \(word)")
+
+let favoriteColor = "cerulean"
+
+print("The sky is not \(favoriteColor)")
+
+print("Hi Mom! My favorite word is \(word), my favorite color is \(favoriteColor), and my favorite musician is \(artist).")
