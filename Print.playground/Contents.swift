5337b02c86b5dff82acc68482076f75de09ac0dc
Carlos <chernandez411@gmail.com>
2016-12-12 18:02:09 -0600

    Submit solution

---
+++ Print.playground/Contents.swift
@@ -0,0 +1,19 @@
+var favoriteWord = "colloquially"
+var favoriteColor = "#4311f9"
+var favoriteMusician = "Renaissance Sound"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is colloquially, my favorite color is #4311f9, and my favorite musician is Renaissance Sound.")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var skySentence = "The sky is \(color)."
+print(skySentence)
