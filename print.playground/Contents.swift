28f37946ad8d53486167220567590472f80ce627
Poncho <polishponcho@gmail.com>
2016-12-14 23:53:43 -0600

    Complete problems

---
+++ print.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var favoriteWord = "lit"
+
+var favoriteColor = "blue"
+
+var favoriteMusician = "Gallant"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("String theory, string cheese, string cheese incident")
+
+print("Hello Mommy! My favorite word is crispy, my favorite color is purple, and my favorite musician is Madonna.")
+
+let color = "blue"
+print("the sky is \(color).")
+
+print("Hey there momma! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var favoriteJedi = "Obi Wan"
+
+var favoriteSith = "Darth Maul"
+
+var favoriteMovie = "Phantom Menace"
+
+print("I was only 10 when I watched \(favoriteJedi) and \(favoriteSith) in an epic battle in the \(favoriteMovie).")
+
+
