a9d2de19da486af70b1c048fd8e6de064dede88d
Rodolfo Delgado <rodolfodelgado@The-Dark-Side-2.local>
2017-01-01 17:47:11 -0500

    adding my playground

---
+++ Memorable name.playground/Contents.swift
@@ -0,0 +1,14 @@
+var favoriteWord = "perfection"
+var favoriteColor = "blue"
+var favoriteMusician = "Muse"
+var sentence = "Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician)."
+
+print("My favorite word is the following:")
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print(sentence)
+
+
