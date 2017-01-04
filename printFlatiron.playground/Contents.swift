c259e46e8422b97d238798cfb0505f9f7514ed28
Michael Florez <mike_florez@ymail.com>
2017-01-04 15:29:39 -0700

    done

---
+++ printFlatiron.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var favWord = "Yo"
+var favColor = "Blue"
+var favMusician = "Nas"
+
+print(favWord)
+print(favColor)
+print(favMusician)
+
+print("I'm printing swift")
+
+print("My fav color is \(favColor.lowercased()). My fav word is \(favWord.capitalized)! and \(favMusician) is my fav rapper!")
+
+
+
+
+
+
+
+
+
+
+
+
+
