588a352622936e475f9bba0fe854634e8985f3dc
Alex Tang <alex.m.tang@gmail.com>
2017-01-17 17:06:25 -0500

    Learn string interpolation

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,5 @@
+var favoriteWord = "transcendence"
+var favoriteColor = "periwinkle blue"
+var favoriteMusician = "Michael Jackson"
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
