3fc586fa3ac36b3a0ed168accb71814d62faa002
Dramalho7 <Dramalho7@gmail.com>
2017-02-10 14:42:10 -0500

    Playground added

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+var favoriteWord = "Crust"
+var favoriteColor = "Blue"
+var favoriteMusician = "Dave Matthews"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("I'm printing a string in Swift!")
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
