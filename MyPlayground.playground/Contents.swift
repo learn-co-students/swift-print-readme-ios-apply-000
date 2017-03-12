5290bda1adde0be013ab560c2ba991f9988d8ece
Bhuvana Raghu <bhuv.raghu@gmail.com>
2017-03-11 21:22:32 -0500

    MyPlayground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var favoriteWord = "serendipity"    // Use your own favorite word!
+var favoriteColor = "white"        // Use your own favorite color!
+var favoriteMusician = "ARRAHMAN"  // Use your own favorite musician!
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var favoritePerson = "husband"
+
+print("Hi World! My favourite person is my \(favoritePerson)!")
