8f4cf3406155ec0cae4fee4c214fe8b4ab8c0480
Mike Kamp <michael.kampouris@gmail.com>
2017-04-06 01:16:11 -0400

    Finished Lab

---
+++ print lab.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var month = "April"
+var day = "5th"
+var year = 2017
+var todaysDate = "Today is \(month) \(day), \(year)"
+
+// This is where printing occurs
+print (month)
+print (day)
+print (year)
+
+print (todaysDate)
+
+/*
+ It is important to print your variables or constants for debugging purposes.
+    Printing something different when a function processes correctly or fails to
+    do what it is suppose to will show you where your code is not functioning
+    properly.
+ */
