8433d5f1084a5ee697d4a81145ef408705d66d46
“Diljeet <jeetds1994@gmail.com>
2017-04-11 19:53:30 -0400

    showed print statements

---
+++ forSubmission.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var favoriteWord = "mercurial"    // Use your own favorite word!
+var favoriteColor = "blue"        // Use your own favorite color!
+var favoriteMusician = "Nirvana"  // Use your own favorite musician!
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("Hi Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+
+
+//////////////////////////////////////////////////
+
+var favoriteSuperHero = "Iron Man"
+
+print("My favorite superhero is \(favoriteSuperHero)")
+
+//Prints: My favorite superhero is Iron Man
